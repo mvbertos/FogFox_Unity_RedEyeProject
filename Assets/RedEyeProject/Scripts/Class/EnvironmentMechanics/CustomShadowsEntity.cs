e95d0ed1f920429bb48babc3a3fcd743d71528d6
Mvbertos <matheus.bertos2@gmail.com>
2021-02-26 01:11:23 -0800

    Added some equation to shadow effect

--- Assets/RedEyeProject/Scripts/Class/EnvironmentMechanics/CustomShadowsEntity.cs
+++ Assets/RedEyeProject/Scripts/Class/EnvironmentMechanics/CustomShadowsEntity.cs
@@ -6,6 +6,8 @@ public class CustomShadowsEntity : MonoBehaviour
 {
     public Transform ShadowsParent;
     public SpriteRenderer Renderer;
+    public int ShadowLayerOrder = 2;
+    public AnimationCurve Curve;
     //public Color ShadowColor; can be used as default shadow in the future
     private Dictionary<GameObject, SpriteRenderer> m_InstantiatedRenderer = new Dictionary<GameObject, SpriteRenderer>();
 
@@ -14,6 +16,8 @@ public class CustomShadowsEntity : MonoBehaviour
     {
         SpriteRenderer shadowInstance = Instantiate<SpriteRenderer>(Renderer, ShadowsParent);
         shadowInstance.color = color;
+        shadowInstance.flipY = true;
+        shadowInstance.sortingOrder = ShadowLayerOrder;
         m_InstantiatedRenderer.Add(whoCalledMethod, shadowInstance);
     }
 
@@ -25,7 +29,13 @@ public class CustomShadowsEntity : MonoBehaviour
             {
                 if (m_InstantiatedRenderer.TryGetValue(lightSource, out SpriteRenderer renderer))
                 {
-                    print(lightSource.name + "/" + renderer.name);
+                    Vector3 position = lightSource.transform.position;
+                    position.Normalize();
+                    float rot_z = (Mathf.Atan2(position.y, position.x) * Mathf.Rad2Deg) - 90;
+                    renderer.transform.rotation = Quaternion.Euler(0f, 0f, rot_z);
+                    float sinPosition = Mathf.Sin(rot_z * 0.0096f) * 0.9f;
+                    renderer.transform.localPosition = rot_z < 0 ? new Vector3((sinPosition * -0.5f) * -1, (sinPosition * -0.5f) * -1, 0) : new Vector3((sinPosition * -0.5f), (sinPosition * -0.5f), 0);
+                    
                 }
             }
             yield return new WaitForEndOfFrame();
